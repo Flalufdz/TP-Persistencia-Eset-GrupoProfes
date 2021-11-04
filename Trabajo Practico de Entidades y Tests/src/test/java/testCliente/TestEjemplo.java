2ad2c296d7a09e82c7781905668f5095dba5c5c6
Flalufdz <flaviaivanafernandez@gmail.com>
2021-11-04 19:02:30 -0300

    TP de Entidades y Test, Facundo Vigo

---
+++ Trabajo Practico de Entidades y Tests/src/test/java/testCliente/TestEjemplo.java
@@ -0,0 +1,19 @@
+package testCliente;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestEjemplo {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
